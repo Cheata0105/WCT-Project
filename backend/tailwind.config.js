6d25ac70801733d2a62c1ec5dbff258bc323aa92
Nam Kimhorng <namkimhorng5599@gmail.com>
2026-05-19 13:09:33 +0700

    Backend

--- backend/tailwind.config.js
+++ backend/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './resources/views/**/*.blade.php',
+    './frontend/src/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: 'class',
   theme: {
